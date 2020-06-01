a4a76ba0dcad36b8f587a88aef47daf5216edcf7
Abenezer <monjo003@morris.umn.edu>
2020-06-01 16:06:00 -0500

    adding routes to the home page using node

---
+++ app.js
@@ -0,0 +1,30 @@
+const ejs = require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+
+app.get('/book', function(req, res){
+    res.render('book');
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
