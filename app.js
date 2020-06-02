6604ec28b22ac0399e2dce35e63feb9a5d80db7b
Abenezer <monjo003@morris.umn.edu>
2020-06-02 16:14:22 -0500

    setting up nodemailler for the contact page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const ejs = require("ejs");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const nodemailer = require("nodemailer");
+const path = require("path");
+const smtpTransport = require('nodemailer-smtp-transport');
+//var config = require('./config'); //[env]
+
 
 
 const app = express();
@@ -27,6 +32,58 @@ app.get('/contact', function(req, res){
 app.get('/book', function(req, res){
     res.render('book');
 });
+
+
+app.post("/send", function(req, res){
+    const output = `
+     <p> You have a new contact request </p>
+     <h3>Contact Details</h3>
+     <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Subject: ${req.body.subject}</li>
+     <ul>
+     <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+    
+      // create reusable transporter object using the default SMTP transport
+      var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        // port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'admastravelnodemailer@gmail.com', // generated ethereal user  --- abe@email.com
+          pass: 'TempPass' // generated ethereal password -- password
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+      }));
+    
+      // send mail with defined transport object
+      let mailOptions = {
+        from: 'admastravelnodemailer@gmail.com', // sender address
+        to: "abenezermonjor@gmail.com, monjo003@morris.umn.edu", // list of receivers
+        subject: "Contact Me", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+      };
+    
+   // send mail with defined transport object
+   transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);   
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+    res.render(__dirname+'/views/contact', {msg:'Email has been sent'});
+  });
+  });
+
+
 
 //if heroku hasn't set up a port use local port
 let port = process.env.PORT;
