93941e91ca3f428df6b2901b348c8e4669768c9a
Abenezer <monjo003@morris.umn.edu>
2020-06-07 13:45:51 -0500

    adding a new post route for booking

--- app.js
+++ app.js
@@ -84,6 +84,59 @@ app.post("/contactPost", function(req, res){
   });
 
 
+  app.post("/bookPost", function(req, res){
+    const output = `
+     <p> You have a new booking request </p>
+     <h3>Booking Information</h3>
+     <ul>
+        <li>First Name: ${req.body.first}</li>
+        <li>Last Name: ${req.body.last}</li>
+        <li>Phone: ${req.body.phone}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Time Frame: ${req.body.timeFrame}</li>
+        <li>Flying From: ${req.body.flyingFrom}</li>
+        <li>Destination: ${req.body.destination}</li>
+     <ul>
+     <h3>More Info</h3>
+      <p>${req.body.message}</p>
+    `;
+    
+      // create reusable transporter object using the default SMTP transport
+      var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        // port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'admastravelnodemailer@gmail.com', // generated ethereal user  --- abe@email.com
+          pass: 'TempPass' // generated ethereal password -- password
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+      }));
+    
+      // send mail with defined transport object
+      let mailOptions = {
+        from: 'admastravelnodemailer@gmail.com', // sender address
+        to: "abenezermonjor@gmail.com, monjo003@morris.umn.edu", // list of receivers
+        subject: "Contact Me - Admas Travel", // Subject line
+        text: "Hello world?", // plain text body
+        html: output // html body
+      };
+    
+   // send mail with defined transport object
+   transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);   
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+    res.render(__dirname+'/views/book', {msg:'Email has been sent'});
+  });
+  });
+
 
 //if heroku hasn't set up a port use local port
 let port = process.env.PORT;
